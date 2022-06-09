50ef638ded2a9eca780f35967dc179588dce9a1a
Bashayer Alharbi <99237409+BashayerAlharbi1@users.noreply.github.com>
2022-06-09 09:06:39 -0600

    Create file1.sh

---
+++ file1.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo docker-compose
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo apt -y install docker-compose
+curl -L "https://github.com/docker/compose/releases/download/$(curl https://github.com/docker/compose/releases | grep -m1 '<a href="/docker/compose/releases/download/' | grep -o 'v[0-9:].[0-9].[0-9]')/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
