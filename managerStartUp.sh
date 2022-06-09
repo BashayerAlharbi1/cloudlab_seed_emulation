de91290488d6370e751b3b1e93d21d3d54b58cd0
Bashayer Alharbi <99237409+BashayerAlharbi1@users.noreply.github.com>
2022-06-09 09:41:53 -0600

    Update managerStartUp.sh

--- managerStartUp.sh
+++ managerStartUp.sh
@@ -12,4 +12,22 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+sudo apt-get update
+sudo docker-compose
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo apt -y install docker-compose
+curl -L "https://github.com/docker/compose/releases/download/$(curl https://github.com/docker/compose/releases | grep -m1 '<a href="/docker/compose/releases/download/' | grep -o 'v[0-9:].[0-9].[0-9]')/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -fs /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+sudo docker swarm init --advertise-addr=$MANAGER_IP
+
+sudo modprobe mpls_router
+
+while [[ $(sudo docker node ls | awk 'END{print NR}') != $1 ]];
+do
+    echo "waiting for all worker nodes to join swarm..."
+    sleep 10
+done
+# Create docker registry
 echo "END -----------------"
