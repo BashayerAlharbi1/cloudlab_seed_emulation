82b8bf2b96d91b9b4d3dba47bb8f2c8956a10707
Bashayer Alharbi <99237409+BashayerAlharbi1@users.noreply.github.com>
2022-06-09 09:04:47 -0600

    Create file2.sh

---
+++ file2.sh
@@ -0,0 +1,12 @@
+
+sudo docker swarm init --advertise-addr=$MANAGER_IP
+
+sudo modprobe mpls_router
+
+while [[ $(sudo docker node ls | awk 'END{print NR}') != $1 ]];
+do
+    echo "waiting for all worker nodes to join swarm..."
+    sleep 10
+done
+# Create docker registry
+sudo docker service create --name registry --publish published=5000,target=5000 registry:2
