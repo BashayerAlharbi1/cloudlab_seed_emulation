f2614a8f1e3e0b580c9052e68508f3ff96ca163b
Bashayer Alharbi <99237409+BashayerAlharbi1@users.noreply.github.com>
2022-06-09 09:28:21 -0600

    Update managerStartUp.sh

--- managerStartUp.sh
+++ managerStartUp.sh
@@ -11,3 +11,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+echo "END -----------------"
